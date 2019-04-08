1c28fa4bcfecdbe27f1abe549db85fde78211ec5
Kajene <au594051@post.au.dk>
2019-04-08 13:24:20 +0200

    Added IT2 and IT5 integrationtest

---
+++ MicroWave.Test.Integration/IT4_Cookcontroller_Display.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroWave.Test.Integration
+{
+    class IT4_Cookcontroller_Display
+    {
+    }
+}
