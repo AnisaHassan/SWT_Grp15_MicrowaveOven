8cf61482f36344f2eea1416c7590f4391a990459
Kajene <au594051@post.au.dk>
2019-04-26 11:09:47 +0200

    added IT9 and IT10

---
+++ MicroWave.Test.Integration/IT9_Light_output.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+namespace Microwave.Test.Integration
+{
+    class IT9_Light_output
+    {
+        private ILight _uut_light;
+
+        [SetUp]
+        public void Setup()
+        {
+            IOutput output = new Output();
+            _uut_light = new Light(output);
+        }
+
+        [Test]
+        public void TurnOn_WasOff_CorrectOutput()
+        {
+            _uut_light.TurnOn();
+        }
+
+        [Test]
+        public void TurnOff_WasOn_CorrectOutput()
+        {
+            _uut_light.TurnOn();
+            _uut_light.TurnOff();
+        }
+
+        [Test]
+        public void TurnOn_WasOn_CorrectOutput()
+        {
+            _uut_light.TurnOn();
+            _uut_light.TurnOn();
+
+        }
+
+        [Test]
+        public void TurnOff_Was_Off_CorrectOutput()
+        {
+            _uut_light.TurnOff(); ;
+        }
+    }
+}
