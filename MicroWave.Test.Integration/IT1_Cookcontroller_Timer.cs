15f298cea89fa8ce72baf52f2f9c96c42e410f3e
kbjerringfelt <karen.bjerringfelt@gmail.com>
2019-04-29 17:22:59 +0200

    Lavet lidt til IT1 og IT2

--- MicroWave.Test.Integration/IT1_Cookcontroller_Timer.cs
+++ MicroWave.Test.Integration/IT1_Cookcontroller_Timer.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     class IT1_Cookcontroller_Timer
     {
+        private IUserInterface _userInterface;
+        private CookController _uut;
+        private IDisplay _display;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private IOutput _output;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            _display = Substitute.For<IDisplay>();
+            _timer = Substitute.For<ITimer>();
+            _userInterface = Substitute.For<IUserInterface>();
+            _output = Substitute.For<IOutput>();
+            _uut = new CookController(_timer, _display, _powerTube);
+
+        }
+
+
+        [Test]
+        public void Cookcontroller_StartCooking_()
+        {
+            //Act
+
+            //Assert
+            _powerTube.Received().TurnOn(50);
+        }
     }
 }
