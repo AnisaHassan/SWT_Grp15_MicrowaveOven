b019058f69d479cde42c468f7819b0223cddd2bb
Anisa Hassan <au592172@post.au.dk>
2019-04-30 10:50:25 +0200

    rettet build fejl i IT2

--- MicroWave.Test.Integration/IT2_CookController.cs
+++ MicroWave.Test.Integration/IT2_CookController.cs
@@ -30,6 +30,7 @@ namespace Microwave.Test.Integration
             _timer = new Timer();
             _userInterface = Substitute.For<IUserInterface>();
             _output = Substitute.For<IOutput>();
+            _powerTube = Substitute.For<IPowerTube>();
 
             _display = new Display(_output);
             _uut = new CookController(_timer, _display, _powerTube);
